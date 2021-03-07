cec591ace9f43588701df68c155b784c3aa18cef
Pallavi P R <palluramachandran@gmail.com>
2021-03-07 16:48:11 +0530

    Adding program to find comparesubstring

--- src/com/pallavi/practice/CompareSubString.java
+++ src/com/pallavi/practice/CompareSubString.java
@@ -4,19 +4,14 @@ public class CompareSubString {
 	public static void main(String[] args) {
 		String firstWord = "cat";
 		String secondWord = "mnj";
-		boolean booleanSameString =false;
-//		char[] firstWords = firstWord.toCharArray();
-//		char[] secondWords = secondWord.toCharArray();
-
+		boolean booleanSameString = false;
 		for (int i = 0; i < firstWord.length(); i++) {
-//			for (int j = 0; j < secondWord.length(); j++) {
-
 			if (secondWord.contains(String.valueOf(firstWord.charAt(i)))) {
-					booleanSameString = true;
-				}
-//			}
+				booleanSameString = true;
+				break;
+			}
 		}
-		System.out.println(booleanSameString?"YES":"NO");
+		System.out.println(booleanSameString ? "YES" : "NO");
 
 	}
 
